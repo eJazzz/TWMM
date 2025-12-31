877c71615352fe566e758b0b8d74e1e2b38628c2
eJazzz <eJazzz@users.noreply.github.com>
2025-12-30 20:37:44 -0500

    Change base path to root for GitHub Pages
    Updated base path for deployment on GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/TWMM/', // This line fixes the blank page on GitHub Pages
+      base: '/', // This line fixes the blank page on GitHub Pages
       server: {
         port: 3000,
         host: '0.0.0.0',
